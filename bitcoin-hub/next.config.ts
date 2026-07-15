de480785587bc2662042ddf6a0f33c900d36be95
heat-a11y <billy.overheat@gmail.com>
2026-07-15 16:01:51 +0800

    Delete bitcoin-hub directory

--- bitcoin-hub/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/bitcoin-hub-',
-  images: { unoptimized: true },
-};
-
-export default nextConfig;
